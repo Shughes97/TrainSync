98ee931decc78cc7d243f29bfddd7a7ce3a3d95e
Sam (Coach Cube) <Samcoachcube@Mac.communityfibre.co.uk>
2026-02-24 18:44:15 +0000

    fix: remove capture attribute so camera roll is accessible on /log

--- app/log/page.tsx
+++ app/log/page.tsx
@@ -125,7 +125,6 @@ export default function LogPage() {
               ref={fileInputRef}
               type="file"
               accept="image/*"
-              capture="environment"
               className="hidden"
               onChange={handleFileChange}
             />
